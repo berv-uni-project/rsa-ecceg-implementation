226f9cff8f18486aa02ce2708c47adda80dd41b2
drestanto <dyas@live.com>
2018-03-27 16:45:28 +0700

    tipe bentukan point of bigInt

---
+++ Point.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+public class Point {
+	public BigInteger x, y; // x = absis, y = ordinat
+	public boolean infinity; // titik O, elemen identitas
+
+	public Point() {
+		x = BigInteger.ZERO;
+		y = BigInteger.ZERO;
+		infinity = false;
+	}
+
+	public String toString(){
+		return "(" + x + ", " + y + ")";
+	}
+}
