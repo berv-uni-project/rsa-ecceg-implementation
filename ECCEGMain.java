6a314ba98732597cb3f6b4f811a34a270e6aaa73
drestanto <dyas@live.com>
2018-03-27 22:44:09 +0700

    repair encrypt and generate key

--- ECCEGMain.java
+++ ECCEGMain.java
@@ -15,25 +15,40 @@ public class ECCEGMain {
         ecc.b = b;
         ecc.p = p;
         ecc.k = k;
-        ECCEG ecceg = new ECCEG(ecc, ecc.getBasePoint());
+        
         // Initiate ended
 
-        String private_key_filepath = "key.pri";
-        String public_key_filepath = "key.pub";
-        ECCEGMain.generateKey(ecceg, private_key_filepath, public_key_filepath);
+        System.out.println("Choose an action : ");
+        System.out.println("1. Generate Key");
+        System.out.println("2. Encrypt File");
+        System.out.println("3. Decrypt File");
+        System.out.print("Input 1/2/3 = ");
+        Scanner s = new Scanner(System.in);
+        int chosen = s.nextInt();
 
-        String plainfile = "datatest.txt";
-        String ciphfile = plainfile + ".ciph";
-        ECCEGMain.encryptFile(ecc, ecceg, plainfile, ciphfile);
-
-        // String private_key_filepath = "key.pri";
-        // String public_key_filepath = "key.pub";
-        // String plainfile = "datatest.txt";
-        // String ciphfile = "datatest.txt.ciph";
-        ECCEGMain.decryptFile(ecc, private_key_filepath, public_key_filepath, plainfile, ciphfile);
+        if (chosen == 1) {
+            String private_key_filepath = "key.pri";
+            String public_key_filepath = "key.pub";
+            ECCEGMain.generateKey(ecc, private_key_filepath, public_key_filepath);
+        }
+        else if (chosen == 2) {
+            String public_key_filepath = "key.pub";
+            String plainfile = "datatest.txt";
+            String ciphfile = plainfile + ".ciph";
+            ECCEGMain.encryptFile(ecc, public_key_filepath, plainfile, ciphfile);
+        }
+        else if (chosen == 3) {
+            String private_key_filepath = "key.pri";
+            String public_key_filepath = "key.pub";
+            String plainfile = "datatest.txt";
+            String ciphfile = "datatest.txt.ciph";
+            ECCEGMain.decryptFile(ecc, private_key_filepath, public_key_filepath, plainfile, ciphfile);
+        }
+        else System.out.println("Wrong choice!!");
     }
 
-    public static void generateKey(ECCEG ecceg, String private_key_filepath, String public_key_filepath) throws Exception {
+    public static void generateKey(ECC ecc, String private_key_filepath, String public_key_filepath) throws Exception {
+        ECCEG ecceg = new ECCEG(ecc, ecc.getBasePoint());
         System.out.print("Private key = ");
         System.out.println(ecceg.getPrivateKey());
         ecceg.savePrivateKey(private_key_filepath);
@@ -44,25 +59,29 @@ public class ECCEGMain {
         System.out.println("Saved in " + public_key_filepath);
     }
 
-    public static void encryptFile(ECC ecc, ECCEG ecceg, String filepath_plain, String filepath_ciph) {
-        FileReader fr = new FileReader();
-        byte[] read = fr.fileToBytes(filepath_plain);
-        System.out.println("---===Plainteks===---");
-        System.out.println(new String(read));
-        System.out.println("---======END======---");
-        System.out.println();
-        List<Pair<Point,Point>> enc = ecceg.encryptBytes(read);
-        System.out.println("---===Cipherteks===---");
-        for (Pair<Point,Point> pp: enc) {
-            System.out.print(String.format("%02x%02x%02x%02x",
-                pp.left.x.intValue(),
-                pp.left.y.intValue(),
-                pp.right.x.intValue(),
-                pp.right.y.intValue()));
-        }
-        System.out.println();
-        System.out.println("---======END======---");
-        fr.savePointsToFile(filepath_ciph, enc);
+    public static void encryptFile(ECC ecc, String public_key_filepath, String filepath_plain, 
+        String filepath_ciph) throws Exception {
+            ECCEG ecceg = new ECCEG(ecc, ecc.getBasePoint());
+            ecceg.loadPublicKey(public_key_filepath);
+            System.out.println("Public key loaded...");
+            FileReader fr = new FileReader();
+            byte[] read = fr.fileToBytes(filepath_plain);
+            System.out.println("---===Plainteks===---");
+            System.out.println(new String(read));
+            System.out.println("---======END======---");
+            System.out.println();
+            List<Pair<Point,Point>> enc = ecceg.encryptBytes(read);
+            System.out.println("---===Cipherteks===---");
+            for (Pair<Point,Point> pp: enc) {
+                System.out.print(String.format("%02x%02x%02x%02x",
+                    pp.left.x.intValue(),
+                    pp.left.y.intValue(),
+                    pp.right.x.intValue(),
+                    pp.right.y.intValue()));
+            }
+            System.out.println();
+            System.out.println("---======END======---");
+            fr.savePointsToFile(filepath_ciph, enc);
     }
 
     public static void decryptFile(ECC ecc, String private_key_filepath, String public_key_filepath, 
@@ -75,7 +94,10 @@ public class ECCEGMain {
             FileReader fr = new FileReader();
             List<Pair<Point,Point>> read_enc = fr.loadPointsFromFile(filepath_ciph);
             List<Point> read_dec = ecceg.decrypt(read_enc);
-            for (Point pp: read_dec) System.out.print((char)ecc.pointToInt(pp).byteValue());
+            System.out.println("---===Plainteks===---");
+            for (Point pp: read_dec)
+                System.out.print((char)ecc.pointToInt(pp).byteValue());
             System.out.println();
+            System.out.println("---======END======---");
     }
 }
